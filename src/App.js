c8c983de54d6540df904219d69d5fbef4958a189
tan86 <jayparmar7072@gmail.com>
2022-09-23 14:49:43 +0530

    asdsad

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 function App() {
   return (
-    <div style={{ opacity: 1, transform: "none", background: "black" }}>
-      <div className="overflow-hidden px-4 sm:px-8">
-        <div className="relative h-[100vh]">
+    <div className="" style={{ opacity: 1, transform: "none", background: "black" }}>
+      <div className="absolute -translate-x-1/2 z-10 -translate-y-1/2 left-1/2" style={{ opacity: 1, transform: "translateX(0px) translateY(-400px) translateZ(0px)" }}>
+        <div className="bg-green w-[1030px] h-[830px] blur-[200px] absolute -translate-x-1/2 -translate-y-1/2 rounded-full"></div>
+      </div>
 
-          <div className="font-goku w-full absolute justify-center text-white items-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <div className="overflow-hidden px-4 sm:px-8">
+        <div className="relative h-[100vh]" >
+          <div className="overflow-hidden">
+            <div className="absolute duration-300 ease-in-out left-[70%] md:left-[70%] top-[12%] md:top-[5%]" style={{ opacity: 1, transform: "none" }}>
+              <div className=" absolute md:w-[128px] w-[100px] z-10 -rotate-45" style={{ top: "20px", left: "0px" }}>
+                <svg className="fill-white " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 88 88" width="50" height="50"><g id="Pentool"><path d="M79,36H76.9766a32.8015,32.8015,0,0,0-21.6-30H72.142a4,4,0,1,0,0-2H48V2a1,1,0,0,0-1-1H41a1,1,0,0,0-1,1V4H15.858a4,4,0,1,0,0,2H32.6232a32.8015,32.8015,0,0,0-21.6,30H9a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V37a1,1,0,0,0-1-1H13.0234A30.815,30.815,0,0,1,40,6.2631V8a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V6.2631A30.815,30.815,0,0,1,74.9766,36H73a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V37A1,1,0,0,0,79,36Z" /><path d="M46.8945,13.5527A1,1,0,0,0,45,14V46.41a1,1,0,0,0,.5947.914,4,4,0,1,1-3.1894,0A1,1,0,0,0,43,46.41V14a1,1,0,0,0-1.8945-.4473l-19,38a1.0008,1.0008,0,0,0,0,.8946L29.3818,67H58.6182l7.2763-14.5527a1.0008,1.0008,0,0,0,0-.8946Z" /><rect x="26" y="69" width="36" height="8" rx="1" ry="1" /><path d="M29,86a1,1,0,0,0,1,1H58a1,1,0,0,0,1-1V79H29Z" /></g></svg>
+              </div>
+            </div>
+            <div className="absolute duration-300 ease-in-out md:left-[80%] left-[90%] md:top-[89%] top-[91%]" style={{ opacity: 1, transform: "none" }}>
+              <div className=" absolute md:w-[128px] w-[100px] z-10 " style={{ top: "-10px", left: "0px;" }}>
+                <svg className="fill-white " height="50" width="50" viewBox="0 0 88 88" xmlns="http://www.w3.org/2000/svg"><g id="Paint_brush" data-name="Paint brush"><path d="m26.3066 61.791c-3.9795-3.6279-10.7031-5.1553-15.3232-.8389-7.7646 7.253-8.8355 24.27-8.8774 24.99a1 1 0 0 0 1.7309.7383c4.9741-5.3526 8.7051-4.9366 12.311-4.53 3.5962.4014 7.3121.8193 11.3555-4.4024 4.981-6.4316 2.4683-12.6162-1.1968-15.957z" /><path d="m29.451 47.497 11.143 11.443 5.636-4.948-11.96-12.283z" /><path d="m82.3477 4.6572a11.9186 11.9186 0 0 0 -8.5972-3.6572h-.0005a11.8892 11.8892 0 0 0 -8.5786 3.6377c-.229.2353-.4473.4814-.648.7285l-28.9674 34.7975 12.1785 12.5072 33.9227-29.7842a12.4157 12.4157 0 0 0 4.2285-8.9394 12.6491 12.6491 0 0 0 -3.538-9.2901z" /><path d="m21.6088 56.918a15.6448 15.6448 0 0 1 6.0455 3.3955 14.2793 14.2793 0 0 1 4.0716 6.413l7.3641-6.4652-10.925-11.2191z" /></g></svg>
+              </div>
+            </div>
+            <div className="absolute duration-300 ease-in-out md:left-[14%] left-[8%] top-[65%]" style={{ opacity: 1, transform: "none" }}>
+              <div className=" absolute md:w-[128px] w-[100px] z-10" style={{ top: "0px", left: "0px" }}>
+                <svg className="fill-white" height="50" width="50" viewBox="0 0 88 88" xmlns="http://www.w3.org/2000/svg"><g id="Color_Pallete" data-name="Color Pallete"><circle cx="40" cy="73" r="6" /><circle cx="40" cy="15" r="6" /><path d="m55.7568 59.7573a6 6 0 1 0 8.4864 0 6.008 6.008 0 0 0 -8.4864 0z" /><path d="m71.3652 14.4014a41.72 41.72 0 0 0 -30.7461-13.4014 42.1863 42.1863 0 0 0 -31.4462 14.1714 17.2886 17.2886 0 0 0 -1.001 21.2686 33.313 33.313 0 0 0 7.7666 7.5352c5.9345 4.583 10.622 8.2026 6.0088 20.6777l-.0586.1411a15.8043 15.8043 0 0 0 1.0156 13.8335 16.0378 16.0378 0 0 0 11.63 7.9322 42.273 42.273 0 0 0 6.0917.4403q1.6332 0 3.2705-.125c20.8235-1.5923 37.5908-18.7363 39.0035-39.8794a43.0068 43.0068 0 0 0 -11.5348-32.5942zm-17.0224 3.9419a7.9961 7.9961 0 1 1 0 11.3134 8.01 8.01 0 0 1 0-11.3134zm-28.6856 11.3134a8 8 0 1 1 0-11.3134 7.9754 7.9754 0 0 1 0 11.3134zm14.3428 51.3433a8 8 0 1 1 8-8 8.0092 8.0092 0 0 1 -8 8zm0-58a8 8 0 1 1 8-8 8.0092 8.0092 0 0 1 -8 8zm25.6572 46.6567a8 8 0 1 1 0-11.3134 7.9754 7.9754 0 0 1 0 11.3134zm3.3428-17.6567a8 8 0 1 1 8-8 8.0092 8.0092 0 0 1 -8 8z" /><path d="m20 18.0029a5.9971 5.9971 0 1 0 4.2432 1.7544 5.9832 5.9832 0 0 0 -4.2432-1.7544z" /><circle cx="69" cy="44" r="6" /><path d="m64.2432 28.2427a6 6 0 1 0 -8.4864 0 6.008 6.008 0 0 0 8.4864 0z" /></g></svg>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="font-goku w-full absolute justify-center text-white items-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 ">
             <div className="md:-space-y-[77px] -space-y-[12vw] text-[20vw] sm:text-[100px] sm:-space-y-[60px] md:text-[128px]">
-              <div className="flex justify-center items-center">
+              <div className="flex justify-center items-center ">
                 <h2 className="flex justify-center" style={{ opacity: 1 }}>Shivam</h2>
-                <span className="absolute -z-10  -translate-x-8 -translate-y-8 duration-300 ease-linear transform  text-fill-transparent text-stroke text-green" style={{ opacity: 1, transform: "translateX(-23px) translateY(-23px) translateZ(0px)" }}>Shivam</span>
+                <span className="absolute -z-10  -translate-x-8 -translate-y-8 duration-1000 ease-linear transform  text-fill-transparent text-stroke text-green" style={{ opacity: 1, transform: "translateX(-23px) translateY(-23px) translateZ(0px)" }}>Shivam</span>
               </div>
               <div className="flex justify-center" style={{ opacity: 1 }}> <h2>Suthar</h2></div>
             </div>
@@ -17,40 +39,40 @@ function App() {
             </div>
           </div>
 
-          <div class="absolute -translate-x-1/2 -translate-y-1/2 top-[93%] left-1/2 py-40">
-            <div class="flex items-center relative md:space-x-10 space-x-6">
-              <div class="duration-300 ease-linear transform space-x-4 w-[30px] h-[30px] rounded-full flex justify-center items-center">
-                <div class="fill-white duration-300 ease-linear transform  w-[30px] h-[30px] rounded-full flex justify-center items-center">
+          <div className="absolute -translate-x-1/2 -translate-y-1/2 top-[93%] left-1/2 py-40">
+            <div className="flex items-center relative md:space-x-10 space-x-6">
+              <div className="duration-300 ease-linear transform space-x-4 w-[30px] h-[30px] rounded-full flex justify-center items-center">
+                <div className="fill-white duration-300 ease-linear transform  w-[30px] h-[30px] rounded-full flex justify-center items-center">
                   <svg id="behance" height="30" width="30" enable-background="new 0 0 49.652 49.652" viewBox="0 0 49.652 49.652" xmlns="http://www.w3.org/2000/svg"><g><g><g><path d="m20.894 20.686c0-1.9-1.293-1.9-1.293-1.9h-.685-4.634-.002v4.078h4.991c.863 0 1.623-.275 1.623-2.178z" /><path d="m33.221 22.864c-2.821 0-3.215 2.814-3.215 2.814h6.002c0 .002.037-2.814-2.787-2.814z" /><path d="m19.601 25.68h-5.318v4.886h4.711c.08 0 .198.004.339 0 .757-.019 2.196-.235 2.196-2.374 0-2.536-1.928-2.512-1.928-2.512z" /><path d="m24.826 0c-13.689 0-24.826 11.137-24.826 24.826 0 13.688 11.137 24.826 24.826 24.826 13.688 0 24.826-11.138 24.826-24.826 0-13.689-11.136-24.826-24.826-24.826zm4.393 16.615h7.539v2.251h-7.539zm-3.812 11.804c0 5.577-5.806 5.395-5.806 5.395h-5.318-.156-4.034v-18.275h4.034.156 5.318c2.891 0 5.172 1.597 5.172 4.867 0 3.269-2.788 3.477-2.788 3.477 3.675-.001 3.422 4.536 3.422 4.536zm14.105-.3h-9.471c0 3.396 3.217 3.182 3.217 3.182 3.036 0 2.93-1.966 2.93-1.966h3.219c0 5.218-6.254 4.859-6.254 4.859-7.503 0-7.021-6.985-7.021-6.985s-.007-7.022 7.021-7.022c7.396 0 6.357 7.932 6.359 7.932z" /></g></g></g></svg>
                 </div>
-                <div class="fill-white duration-300 ease-linear transform  w-[30px] h-[30px] rounded-full flex justify-center items-center">
+                <div className="fill-white duration-300 ease-linear transform  w-[30px] h-[30px] rounded-full flex justify-center items-center">
                   <svg id="twitter" height="30" width="30" enable-background="new 0 0 49.652 49.652" viewBox="0 0 49.652 49.652" xmlns="http://www.w3.org/2000/svg"><g><g><path d="m24.826 0c-13.689 0-24.826 11.137-24.826 24.826 0 13.688 11.137 24.826 24.826 24.826 13.688 0 24.826-11.138 24.826-24.826 0-13.689-11.136-24.826-24.826-24.826zm11.075 19.144c.011.246.017.494.017.742 0 7.551-5.746 16.255-16.259 16.255-3.227 0-6.231-.943-8.759-2.565.447.053.902.08 1.363.08 2.678 0 5.141-.914 7.097-2.446-2.5-.046-4.611-1.698-5.338-3.969.348.066.707.103 1.074.103.521 0 1.027-.068 1.506-.199-2.614-.524-4.583-2.833-4.583-5.603 0-.024 0-.049.001-.072.77.427 1.651.685 2.587.714-1.532-1.023-2.541-2.773-2.541-4.755 0-1.048.281-2.03.773-2.874 2.817 3.458 7.029 5.732 11.777 5.972-.098-.419-.147-.854-.147-1.303 0-3.155 2.558-5.714 5.713-5.714 1.644 0 3.127.694 4.171 1.804 1.303-.256 2.523-.73 3.63-1.387-.43 1.335-1.333 2.454-2.516 3.162 1.157-.138 2.261-.444 3.282-.899-.762 1.144-1.731 2.151-2.848 2.954z" /></g></g></svg>
                 </div>
-                <div class="fill-white duration-300 ease-linear transform  w-[30px] h-[30px] rounded-full flex justify-center items-center">
+                <div className="fill-white duration-300 ease-linear transform  w-[30px] h-[30px] rounded-full flex justify-center items-center">
                   <svg id="linkedin" height="30" width="30" viewBox="0 0 152 152" xmlns="http://www.w3.org/2000/svg"><g id="Layer_2" data-name="Layer 2"><g id="Color"><path d="m76 0a76 76 0 1 0 76 76 76 76 0 0 0 -76-76zm-22.1 116h-16.58v-53.41h16.58zm-8.3-60.7a9.65 9.65 0 1 1 9.61-9.7 9.68 9.68 0 0 1 -9.61 9.7zm70.4 60.7h-16.57v-26c0-6.2-.12-14.15-8.62-14.15s-10 6.74-10 13.7v26.45h-16.51v-53.41h15.91v7.28h.23c2.21-4.2 7.62-8.63 15.69-8.63 16.78 0 19.87 11.06 19.87 25.42z" data-name="10.Linkedin" /></g></g></svg>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="wrapper mx-10">
-            <div class="fixed block lg:hidden z-[10000000] cursor-pointer -translate-y-[0%] bottom-10 -translate-x-1/2 -mx-[19px] w-4 h-4 bg-green rounded-full"></div>
-            <div class="fixed z-30 duration-300 ease-in-out delay-200 w-[0.5px] h-[260px] bg-white/20  -translate-y-[0%] lg:-translate-y-1/2 -bottom-[500px]  lg:top-1/2 -translate-x-1/2 -mx-[19px]">
-              <div class="space-y-12 flex flex-col justify-center items-center">
-                <div class=" ">
-                  <div class="h-[9px]  bigger relative w-[9px] mr-0 transform ease-in-out duration-300 cursor-pointer  bg-red rounded-full"></div>
+          <div className="wrapper mx-10">
+            <div className="fixed block lg:hidden z-[10000000] cursor-pointer -translate-y-[0%] bottom-10 -translate-x-1/2 -mx-[19px] w-4 h-4 bg-green rounded-full"></div>
+            <div className="fixed z-30 duration-300 ease-in-out delay-200 w-[0.5px] h-[260px] bg-white/20  -translate-y-[0%] lg:-translate-y-1/2 -bottom-[500px]  lg:top-1/2 -translate-x-1/2 -mx-[19px]">
+              <div className="space-y-12 flex flex-col justify-center items-center">
+                <div className=" ">
+                  <div className="h-[9px]  bigger relative w-[9px] mr-0 transform ease-in-out duration-300 cursor-pointer  bg-red rounded-full"></div>
                 </div>
-                <div class="">
-                  <div class="h-[9px] bigger relative w-[9px] mr-0 transform ease-in-out duration-300 cursor-pointer z-50 bg-purple rounded-full"></div>
+                <div className="">
+                  <div className="h-[9px] bigger relative w-[9px] mr-0 transform ease-in-out duration-300 cursor-pointer z-50 bg-purple rounded-full"></div>
                 </div>
-                <div class="">
-                  <div class="h-[9px] bigger  relative w-[9px] mr-0 transform ease-in-out duration-300 cursor-pointer z-50 bg-blue rounded-full"></div>
+                <div className="">
+                  <div className="h-[9px] bigger  relative w-[9px] mr-0 transform ease-in-out duration-300 cursor-pointer z-50 bg-blue rounded-full"></div>
                 </div>
-                <div class="">
-                  <div class="h-[9px]  bigger relative w-[9px] mr-0 transform ease-in-out duration-300 cursor-pointer z-50 bg-yellow rounded-full"></div>
+                <div className="">
+                  <div className="h-[9px]  bigger relative w-[9px] mr-0 transform ease-in-out duration-300 cursor-pointer z-50 bg-yellow rounded-full"></div>
                 </div>
-                <div class="border-[1px] border-white bg-main rounded-full">
-                  <div class="h-[15px] bigger  relative w-[15px] transform ease-in-out duration-300 m-2 cursor-pointer z-50 bg-green rounded-full"></div>
+                <div className="border-[1px] border-white bg-main rounded-full">
+                  <div className="h-[15px] bigger  relative w-[15px] transform ease-in-out duration-300 m-2 cursor-pointer z-50 bg-green rounded-full"></div>
                 </div>
               </div>
             </div>
@@ -58,8 +80,13 @@ function App() {
 
         </div>
       </div>
-    </div>
+    </div >
   );
 }
-
+/*
+<div className="sm:block hidden">
+        <div id="dotOutline" className="absolute z-20 w-14 h-14 border-[0.5px] top-1/2 left-1/2 rounded-full -translate-y-1/2 opacity-100 transition ease-in-out -translate-x-1/2 " style={{ top: { x }, left: { y }, transform: "translate(-50%, -50%) scale(1)" }}></div>
+        <div id="dot" className="absolute z-20 w-[0.3rem] h-[0.3rem] bg-green top-1/2 left-1/2 rounded-full -translate-y-1/2 opacity-100 transition ease-in-out -translate-x-1/2" style={{ top: { x }, left: { y }, transform: "translate(-50%, -50%) scale(1)" }}></div>
+      </div>
+      */
 export default App;
