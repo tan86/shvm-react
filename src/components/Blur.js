a420e32121e40fdc6444ca0c2ee6fc9d369d6567
tan86 <jayparmar7072@gmail.com>
2022-10-04 16:17:54 +0530

    asdsad

--- src/components/Blur.js
+++ src/components/Blur.js
@@ -1,7 +1,10 @@
 export default function Blur() {
     return (
-        <div className="absolute -translate-x-1/2 z-10 -translate-y-1/2 left-1/2" style={{ opacity: 1, transform: "translateX(0px) translateY(-400px) translateZ(0px)" }}>
-            <div className="bg-theme w-[1030px] h-[830px] blur-[200px] absolute -translate-x-1/2 -translate-y-1/2 rounded-full"></div>
+        <div className="overflow-hidden">
+            <div className="absolute -translate-x-1/2 z-10 -translate-y-1/2 left-1/2" style={{ opacity: 1, transform: "translateX(0px) translateY(-400px) translateZ(0px)" }}>
+                <div className="bg-theme w-[1030px] h-[830px] blur-[200px] absolute -translate-x-1/2 -translate-y-1/2 rounded-full"></div>
+            </div>
         </div>
+
     );
 }
