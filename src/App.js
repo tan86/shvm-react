819100f8096c1177223a8b79c5b522b554e8a365
tan86 <jayparmar7072@gmail.com>
2022-10-11 20:19:09 +0530

    jkjl

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Contact from "./components/Contact";
 export default function App() {
   return (
     <div className="m-auto" style={{ opacity: 1, transform: "none", background: "black" }}>
+      <Blur />
       <div className="overflow-hidden px-4 sm:px-8">
         <AnimatedCursor outerSize={8} outerScale={5} />
-        <Blur />
         <Cover />
         <Projects />
         <Exp />
